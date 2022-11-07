de601cfcb0b30d797afc79caa3a42ce7426197c8
Sekwah <contact@sekwah.com>
2022-11-07 12:39:00 +0000

    feat: download information for stores & add easier model list

--- README.md
+++ README.md
@@ -3,4 +3,10 @@ Based on https://github.com/insanoid/Apple-Store-Reserve-Monitor
 
 Though I rewrote it as it was having issues parsing some of the json.
 
-See https://github.com/insanoid/Apple-Store-Reserve-Monitor for more info
+There are also some edits to the config to make it easier to find the devices e.g. you can search by product title and itll partially match it.
+
+E.g. if you want a 'iPhone 14 Pro Max 512GB' and dont care about color you'd just ignore the color at the end.
+
+See https://github.com/insanoid/Apple-Store-Reserve-Monitor for more info.
+
+Also rather than a noise I will likely trigger a webhook to alert via a service like discord.
--- store_checker.js
+++ store_checker.js
@@ -2,6 +2,8 @@ import fetch from 'node-fetch';
 import fs from 'fs';
 // Adds extra stuff to string prototype
 import colors from 'colors';
+import ProgressBar from "progress";
+import { PromiseQueue } from 'spq';
 
 class Configuration {
     constructor(filename) {
@@ -12,9 +14,10 @@ class Configuration {
         const config = JSON.parse(fs.readFileSync(filename, 'utf8'));
         this.country_code = config.country_code;
         this.device_family = config.device_family;
-        this.zip_code = config.zip_code | [];
+        this.zip_code = config.zip_code || [];
         this.selected_device_models = config.models || [];
         this.selected_carriers = config.carriers || [];
+        this.product = config.product || [];
         this.selected_stores = config.stores || [];
         this.appointment_stores = config.appointment_stores;
     }
@@ -27,13 +30,13 @@ class StoreChecker {
         this.PRODUCT_LOCATOR_URL = `${this.base_url}shop/product-locator-meta?family=${configuration.device_family}`;
         this.PRODUCT_AVAILABILITY_URL = (device) => `${this.base_url}shop/retail/pickup-message?pl=true&parts.0=${device}&location=${configuration.zip_code}`;
         //this.STORE_APPOINTMENT_AVAILABILITY_URL = `https://retail-pz.cdn-apple.com/product-zone-prod/availability/{0}/{1}/availability.json`;
-        //this.stores_list_with_stock = {};
+        this.stores_list_with_stock = {};
     }
 
     async refresh() {
         const device_list = await this.find_devices();
 
-        if(device_list) {
+        if (device_list) {
             console.log("✔ Found".green, `${device_list.length}`.cyan, "devices matching your config.".green)
         } else {
             console.log("❌ No devices found matching your config.".red);
@@ -41,6 +44,94 @@ class StoreChecker {
         }
 
         console.log("➜ Downloading Stock Information for the devices...".blue);
+
+        const bar = new ProgressBar(`:current/:total ${'['.red}${':bar'.green}${']'.red} :percent`.cyan, {total: device_list.length});
+
+        const customQueue = new PromiseQueue(2);
+        const QueuedTask = customQueue.QueuedTask;
+
+        let promises = [];
+        for (const device of device_list) {
+            promises.push(QueuedTask(async (resolve) => {
+                await this.check_stores_for_device(device)
+                bar.tick();
+                resolve();
+            }));
+        }
+
+        await Promise.all(promises);
+
+        const stores = Object.values(this.stores_list_with_stock).sort((a, b) => {
+            return a.sequence - b.sequence;
+        });
+
+        let stock_available = false;
+
+        for (const store of stores) {
+            console.log(`\n\n${store.storeName.green}, ${store.city.green}, (${store.storeId.green})`);
+        }
+    }
+
+
+
+    async delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async check_stores_for_device(device) {
+        //console.log(device);
+        const url = this.PRODUCT_AVAILABILITY_URL(device.model);
+        //console.log("\n" + url);
+        let availability;
+
+        let attempts = 10;
+
+        while (attempts-- > 0) {
+            availability = await fetch(url);
+
+            if(availability.status === 200) {
+                attempts = 0;
+            } else {
+                await this.delay(10000);
+            }
+        }
+
+
+
+        if(availability.status !== 200) {
+            console.log(`❌ Error fetching one or more devices, code: ${availability.status}`.red);
+            process.exit(0);
+        }
+
+        const availabilityJson = await availability.json();
+
+        const store_list = availabilityJson.body.stores;
+
+        for(let store of store_list) {
+            let current_store = this.stores_list_with_stock[store.storeNumber];
+            if(!current_store) {
+                current_store = {
+                    "storeId": store.storeNumber,
+                    "storeName": store.storeName,
+                    "city": store.city,
+                    "sequence": store.storeListNumber,
+                    "parts": {},
+                }
+            }
+            let new_parts = store.partsAvailability
+            let old_parts = current_store.parts;
+            old_parts = new_parts;
+            current_store["parts"] = old_parts;
+            //If the store is in the list of user's preferred list, add it to the
+            // list to check for stock.
+            if (
+                this.configuration.selected_stores.includes(store.storeNumber)
+                || this.configuration.selected_stores.length == 0
+                 ){
+                this.stores_list_with_stock[store.storeNumber] = current_store;
+            }
+
+        }
     }
 
     async find_devices() {
@@ -57,14 +148,19 @@ class StoreChecker {
             product_list.forEach(product => {
                 if (this.configuration.selected_device_models.length === 0 || this.configuration.selected_device_models.includes(product.model)) {
                     if (this.configuration.selected_carriers.length === 0 || this.configuration.selected_carriers.includes(product.carrier)) {
-                        device_list.push({
-                            "title": product.productTitle,
-                            "model": product.model,
-                            "carrier": product.carrier
-                        });
+                        if (this.configuration.product.length === 0 || this.configuration.product.find((item) => product.productTitle.toLowerCase().includes(item.toLowerCase()))) {
+                            //console.log(product);
+                            device_list.push({
+                                "title": product.productTitle,
+                                "model": product.partNumber,
+                                "carrier": product.carrierModel
+                            });
+                        }
                     }
                 }
             });
+
+            console.log("Looking for stock for the following models: ".blue, `\n${device_list.map(device => device.title).join(',\n')}`.cyan);
         } catch (e) {
             console.log("❌ Unable to download models list.".red);
             console.warn(e);
